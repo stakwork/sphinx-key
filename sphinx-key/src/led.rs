88439f169a5819345f14f962a7edb00366062b68
Evan Feenstra <evanfeenstra@gmail.com>
2022-05-23 17:24:16 -0700

    RGB led (neopixel) blinky sketch

---
+++ sphinx-key/src/led.rs
@@ -0,0 +1,40 @@
+use core::time::Duration;
+use embedded_hal::delay::blocking::DelayUs;
+use esp_idf_hal::delay::Ets;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::rmt::config::TransmitConfig;
+use esp_idf_hal::rmt::{FixedLengthSignal, PinState, Pulse, Transmit};
+
+fn main() -> anyhow::Result<()> {
+    esp_idf_sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    let led = peripherals.pins.gpio8.into_output()?;
+    let channel = peripherals.rmt.channel0;
+    let config = TransmitConfig::new().clock_divider(1);
+    let mut tx = Transmit::new(led, channel, &config)?;
+
+    let rgbs = [0x000000, 0x010001]; // grb?
+    loop {
+        for rgb in rgbs {
+            let ticks_hz = tx.counter_clock()?;
+            let t0h = Pulse::new_with_duration(ticks_hz, PinState::High, &ns(350))?;
+            let t0l = Pulse::new_with_duration(ticks_hz, PinState::Low, &ns(800))?;
+            let t1h = Pulse::new_with_duration(ticks_hz, PinState::High, &ns(700))?;
+            let t1l = Pulse::new_with_duration(ticks_hz, PinState::Low, &ns(600))?;
+
+            let mut signal = FixedLengthSignal::<24>::new();
+            for i in 0..24 {
+                let bit = 2_u32.pow(i) & rgb != 0;
+                let (high_pulse, low_pulse) = if bit { (t1h, t1l) } else { (t0h, t0l) };
+                signal.set(i as usize, &(high_pulse, low_pulse))?;
+            }
+            tx.start_blocking(&signal)?;
+            Ets.delay_ms(200)?;
+        }
+    }
+}
+
+fn ns(nanos: u64) -> Duration {
+    Duration::from_nanos(nanos)
+}
