9ccaf043f57d47fc366d80bf2fad2a3282c60db6
Evan Feenstra <evanfeenstra@gmail.com>
2022-06-14 12:27:04 -0700

    secp256k1 mre

--- sphinx-key/src/tiny.rs
+++ sphinx-key/src/tiny.rs
@@ -1,28 +1,11 @@
 #![feature(once_cell)]
 
-use lightning_signer::node::{NodeConfig};
-use lightning_signer::signer::derive::KeyDerivationStyle;
-use lightning_signer::signer::my_keys_manager::MyKeysManager;
-use std::time::Duration;
-use sphinx_key_signer::lightning_signer;
-use sphinx_key_signer::lightning_signer::bitcoin::Network;
+use sphinx_key_signer::lightning_signer::bitcoin::secp256k1::Secp256k1;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
-    
-    let network = Network::Regtest;
-    let config = NodeConfig { network, key_derivation_style: KeyDerivationStyle::Native };
 
-    let seed = [0; 32];
-
-    let now = Duration::from_secs(1);
-    let keys_manager = MyKeysManager::new(
-        config.key_derivation_style,
-        &seed[..],
-        config.network,
-        now.as_secs(),
-        now.subsec_nanos(),
-    );
+    let ctx = Secp256k1::new();
 
     Ok(())
 }
