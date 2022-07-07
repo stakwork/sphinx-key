976b602b12dcf88dad8d80dc15f7e8705c6576a8
Evan Feenstra <evanfeenstra@gmail.com>
2022-07-07 11:52:24 -0700

    update build-kotlin script

--- crypter-ffi/build-kotlin.sh
+++ crypter-ffi/build-kotlin.sh
@@ -10,3 +10,14 @@ echo "=> building aarch64-linux-android"
 cross build --target aarch64-linux-android --release
 echo "=> building armv7-linux-androideabi"
 cross build --target armv7-linux-androideabi --release
+
+echo "=> renaming files"
+
+mv target/aarch64-linux-android/release/libcrypter.a target/aarch64-linux-android/release/aarch64-libcrypter.a
+mv target/aarch64-linux-android/release/libcrypter.so target/aarch64-linux-android/release/aarch64-libcrypter.so
+
+mv target/armv7-linux-androideabi/release/libcrypter.a target/armv7-linux-androideabi/release/armv7-libcrypter.a
+mv target/armv7-linux-androideabi/release/libcrypter.so target/armv7-linux-androideabi/release/armv7-libcrypter.so
+
+mv target/i686-linux-android/release/libcrypter.a target/i686-linux-android/release/i686-libcrypter.a
+mv target/i686-linux-android/release/libcrypter.so target/i686-linux-android/release/i686-libcrypter.so
