d42d6093e62b2a7c4f568236dc1eef9aca3481af
Evan Feenstra <evanfeenstra@gmail.com>
2022-07-07 12:39:30 -0700

    add x86_64-linux-android and arm-linux-androideabi

--- crypter-ffi/build-kotlin.sh
+++ crypter-ffi/build-kotlin.sh
@@ -8,16 +8,24 @@ echo "=> building i686-linux-android"
 cross build --target i686-linux-android --release
 echo "=> building aarch64-linux-android"
 cross build --target aarch64-linux-android --release
+echo "=> building arm-linux-androideabi"
+cross build --target arm-linux-androideabi --release
 echo "=> building armv7-linux-androideabi"
 cross build --target armv7-linux-androideabi --release
+echo "=> building x86_64-linux-android"
+cross build --target x86_64-linux-android --release
 
 echo "=> renaming files"
 
-mv target/aarch64-linux-android/release/libcrypter.a target/aarch64-linux-android/release/aarch64-libcrypter.a
-mv target/aarch64-linux-android/release/libcrypter.so target/aarch64-linux-android/release/aarch64-libcrypter.so
+mkdir -p target/out
 
-mv target/armv7-linux-androideabi/release/libcrypter.a target/armv7-linux-androideabi/release/armv7-libcrypter.a
-mv target/armv7-linux-androideabi/release/libcrypter.so target/armv7-linux-androideabi/release/armv7-libcrypter.so
+mv target/i686-linux-android/release/libcrypter.so target/out/i686-libcrypter.so
 
-mv target/i686-linux-android/release/libcrypter.a target/i686-linux-android/release/i686-libcrypter.a
-mv target/i686-linux-android/release/libcrypter.so target/i686-linux-android/release/i686-libcrypter.so
+mv target/aarch64-linux-android/release/libcrypter.so target/out/aarch64-libcrypter.so
+
+mv target/arm-linux-androideabi/release/libcrypter.so target/out/arm-libcrypter.so
+
+mv target/armv7-linux-androideabi/release/libcrypter.so target/out/armv7-libcrypter.so
+
+mv target/x86_64-linux-android/release/libcrypter.so target/out/x86_64-libcrypter.so
+
--- crypter-ffi/readme.md
+++ crypter-ffi/readme.md
@@ -8,13 +8,15 @@ uniffi-bindgen scaffolding src/crypter.udl
 
 ### kotlin
 
-rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android x86_64-unknown-linux-gnu
+rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android arm-linux-androideabi 
 
 uniffi-bindgen generate src/crypter.udl --language kotlin
 
 cross build --target i686-linux-android --release
 cross build --target aarch64-linux-android --release
+cross build --target arm-linux-androideabi --release
 cross build --target armv7-linux-androideabi --release
+cross build --target x86_64-linux-android --release
 
 ### swift
 
