5de335c797fa85b01e9cb1901250f29cbd5b233f
Evan Feenstra <evanfeenstra@gmail.com>
2022-09-15 16:12:34 -0700

    more clears

--- sphinx-key/src/clear.rs
+++ sphinx-key/src/clear.rs
@@ -11,6 +11,9 @@ fn main() -> anyhow::Result<()> {
     let mut store =
         EspNvsStorage::new_default(default_nvs.clone(), "sphinx", true).expect("no storage");
     store.remove("config").expect("couldnt remove config 1");
+    store.remove("seed").expect("couldnt remove seed 1");
+    store.remove("nonce").expect("couldnt remove nonce 1");
+    store.remove("policy").expect("couldnt remove policy 1");
     println!("NVS cleared!");
     Ok(())
 }
