d41a5cc3f55b9b1d057247f6f881556a2123b989
Evan Feenstra <evanfeenstra@gmail.com>
2022-10-12 15:16:20 -0400

    release in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -50,7 +50,7 @@ git pull
 cd factory
 cargo espflash --release $PORT
 cd ../sphinx-key
-cargo build
-esptool.py --chip esp32-c3 elf2image target/riscv32imc-esp-espidf/debug/sphinx-key
-esptool.py --chip esp32c3 -b 460800 --before=default_reset --after=hard_reset write_flash --flash_mode dio --flash_freq 40m --flash_size 4MB 0x80000 target/riscv32imc-esp-espidf/debug/sphinx-key.bin
+cargo build --release
+esptool.py --chip esp32-c3 elf2image target/riscv32imc-esp-espidf/release/sphinx-key
+esptool.py --chip esp32c3 -b 460800 --before=default_reset --after=hard_reset write_flash --flash_mode dio --flash_freq 40m --flash_size 4MB 0x80000 target/riscv32imc-esp-espidf/release/sphinx-key.bin
 cargo espflash serial-monitor $PORT
