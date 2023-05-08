35b7efeb96ffece367499111b8200d578181f298
Evan Feenstra <evanfeenstra@gmail.com>
2023-05-08 09:13:47 +0100

    track button state

--- sphinx-key/src/periph/button.rs
+++ sphinx-key/src/periph/button.rs
@@ -9,14 +9,21 @@ pub fn button_loop(gpio8: gpio::Gpio8, tx: mpsc::Sender<Status>) {
     thread::spawn(move || {
         let mut button = PinDriver::input(gpio8).unwrap();
         button.set_pull(Pull::Down).unwrap();
+        let mut high = false;
         loop {
             // we are using thread::sleep here to make sure the watchdog isn't triggered
-            thread::sleep(Duration::from_millis(10));
+            thread::sleep(Duration::from_millis(15));
             if button.is_high() {
-                log::info!("=> GPIO8 HIGH!");
+                if !high {
+                    high = true;
+                    log::info!("=> GPIO8 HIGH!");
+                }
                 tx.send(Status::Reset1).unwrap();
             } else {
-                log::info!("=> GPIO8 LOW!");
+                if high {
+                    high = false;
+                    log::info!("=> GPIO8 LOW!");
+                }
             }
         }
     });
