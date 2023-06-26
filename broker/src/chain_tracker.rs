d407ccfe35d7e8dd867f0202b042299b74066114
Evan Feenstra <evanfeenstra@gmail.com>
2023-06-26 09:31:32 -0700

    use SerialRequestHeader for frontend msgs

--- broker/src/chain_tracker.rs
+++ broker/src/chain_tracker.rs
@@ -1,7 +1,7 @@
 use crate::conn::{ChannelReply, ChannelRequest};
 use async_trait::async_trait;
 use rocket::tokio::sync::{mpsc, oneshot};
-use sphinx_signer::sphinx_glyph::topics;
+use sphinx_signer::{parser, sphinx_glyph::topics};
 use vls_protocol::{Error, Result};
 use vls_protocol_client::{ClientResult, SignerPort};
 
@@ -27,13 +27,15 @@ impl MqttSignerPort {
     }
 
     async fn send_request(&self, message: Vec<u8>) -> Result<oneshot::Receiver<ChannelReply>> {
-        let (request, reply_rx) = ChannelRequest::new(topics::VLS, message);
+        let m = parser::raw_request_from_bytes(message, 0, [0;33], 0)?;
+        let (request, reply_rx) = ChannelRequest::new(topics::VLS, m);
         self.sender.send(request).await.map_err(|_| Error::Eof)?;
         Ok(reply_rx)
     }
 
     async fn get_reply(&self, reply_rx: oneshot::Receiver<ChannelReply>) -> ClientResult<Vec<u8>> {
         let reply = reply_rx.await.map_err(|_| Error::Eof)?;
-        Ok(reply.reply)
+        let r = parser::raw_response_from_bytes(reply.reply, 0)?;
+        Ok(r)
     }
 }
