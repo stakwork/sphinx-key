1b54c5358c4700b20fc5b725e1e9f19dfecf4514
Evan Feenstra <evanfeenstra@gmail.com>
2023-06-26 10:41:50 -0700

    chaintracker logs

--- broker/src/chain_tracker.rs
+++ broker/src/chain_tracker.rs
@@ -29,13 +29,17 @@ impl MqttSignerPort {
     async fn send_request(&self, message: Vec<u8>) -> Result<oneshot::Receiver<ChannelReply>> {
         let m = parser::raw_request_from_bytes(message, 0, [0;33], 0)?;
         let (request, reply_rx) = ChannelRequest::new(topics::VLS, m);
+        log::info!("=> MqttSignerPort send request now");
         self.sender.send(request).await.map_err(|_| Error::Eof)?;
+        log::info!("=> MqttSignerPort sent");
         Ok(reply_rx)
     }
 
     async fn get_reply(&self, reply_rx: oneshot::Receiver<ChannelReply>) -> ClientResult<Vec<u8>> {
         let reply = reply_rx.await.map_err(|_| Error::Eof)?;
+        log::info!("=> MqttSignerPort got response {:?}", reply.reply);
         let r = parser::raw_response_from_bytes(reply.reply, 0)?;
+        log::info!("=> MqttSignerPort parsed response {:?}", r);
         Ok(r)
     }
 }
